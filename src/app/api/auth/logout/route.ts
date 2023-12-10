0aa32ac65a6dbdb5a51b681a4ec564229b9ab40c
Aman Soni <gitaman8481@gmail.com>
2023-12-10 15:06:07 +0530

    fix: api: deployment issues

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,45 +1,45 @@
-import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+// import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export default async function handler(req: NextRequest) {
-    const reqBody = await req.json();
-    const { userId } = reqBody;
+// export default async function handler(req: NextRequest) {
+//     const reqBody = await req.json();
+//     const { userId } = reqBody;
     
-    try {
-        const updatedUser = await prisma.user.update({
-            where: {
-                id: userId
-            },
-            data: {
-                refreshToken: null,
-                refreshTokenExpiry: null,
-                refreshTokenStatus: "expired",
-                accessToken: null,
-                accessTokenExpiry: null,
-                accessTokenStatus: "expired",
-            },
-        });
+//     try {
+//         const updatedUser = await prisma.user.update({
+//             where: {
+//                 id: userId
+//             },
+//             data: {
+//                 refreshToken: null,
+//                 refreshTokenExpiry: null,
+//                 refreshTokenStatus: "expired",
+//                 accessToken: null,
+//                 accessTokenExpiry: null,
+//                 accessTokenStatus: "expired",
+//             },
+//         });
 
-        NextResponse.next().headers.set("Authorization", ``);
+//         NextResponse.next().headers.set("Authorization", ``);
 
-        const response = NextResponse.json({
-            status: true,
-            data: updatedUser,
-            message: "Logout Successfull 😶 ",
-        }, { status: 200 });
+//         const response = NextResponse.json({
+//             status: true,
+//             data: updatedUser,
+//             message: "Logout Successfull 😶 ",
+//         }, { status: 200 });
 
-        response.cookies.set("token", '');
+//         response.cookies.set("token", '');
 
-        return response;
-    } catch (error) {
-        console.error(error);
-        console.error("Error:", error);
-        return NextResponse.json({
-            status: false,
-            error: error,
-            message: "Failed to logout user ⚠👮‍♂️..",
-        }, { status: 500 });
-    }
-};
+//         return response;
+//     } catch (error) {
+//         console.error(error);
+//         console.error("Error:", error);
+//         return NextResponse.json({
+//             status: false,
+//             error: error,
+//             message: "Failed to logout user ⚠👮‍♂️..",
+//         }, { status: 500 });
+//     }
+// };
