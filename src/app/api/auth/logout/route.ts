657e2a283e60a3c5c9d816b4828dd028eaaae54a
Aman Soni <gitaman8481@gmail.com>
2023-12-10 13:13:31 +0530

    fix: api: deployment issues

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -3,8 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export default async function DELETE(req: NextRequest, res: NextResponse) {
-    
+export default async function handler(req: NextRequest) {
     const reqBody = await req.json();
     const { userId } = reqBody;
     
