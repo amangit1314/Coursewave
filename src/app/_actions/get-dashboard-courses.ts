b5870ae2e7c80b3cb75090f1d2ee469d82855465
amangit1314 <gitaman8481@gmail.com>
2024-03-12 19:34:25 +0530

    fix: deployment error in _actions

--- src/app/_actions/get-dashboard-courses.ts
+++ src/app/_actions/get-dashboard-courses.ts
@@ -1,7 +1,7 @@
 import { Category, Chapter, Course } from "@prisma/client";
 
 import { db } from "@/lib/db";
-import { getProgress } from "@/actions/get-progress";
+import { getProgress } from "./get-progress";
 
 type CourseWithProgressWithCategory = Course & {
   category: Category;
@@ -23,7 +23,7 @@ export const getDashboardCourses = async (userId: string): Promise<DashboardCour
       select: {
         course: {
           include: {
-            category: true,
+            categories: true,
             chapters: {
               where: {
                 isPublished: true,
@@ -34,10 +34,10 @@ export const getDashboardCourses = async (userId: string): Promise<DashboardCour
       }
     });
 
-    const courses = purchasedCourses.map((purchase) => purchase.course) as CourseWithProgressWithCategory[];
+    const courses = purchasedCourses.map((purchase) => purchase.course) as unknown as CourseWithProgressWithCategory[];
 
     for (let course of courses) {
-      const progress = await getProgress(userId, course.id);
+      const progress = await getProgress(userId, course.courseId);
       course["progress"] = progress;
     }
 
