929590f692cfae37689650d9340b68a7290dbc74
amangit1314 <gitaman8481@gmail.com>
2024-04-20 16:47:02 +0530

    fix: deployment errors course publish form solved

--- src/app/(instructor)/instructor/[id]/courses/createdCourses/[courseId]/_components/course-publish-form.tsx
+++ src/app/(instructor)/instructor/[id]/courses/createdCourses/[courseId]/_components/course-publish-form.tsx
@@ -1,41 +1,8 @@
 "use client";
 
-import {
-  CircleDollarSign,
-  Eye,
-  File,
-  LayoutDashboard,
-  ListChecks,
-  Highlighter,
-  Shapes,
-  Video,
-} from "lucide-react";
 import React from "react";
-import { IoMdArrowRoundBack } from "react-icons/io";
-import {
-  Chapter,
-  Course,
-  Category,
-  CourseAttachment,
-  CourseSection,
-} from "@prisma/client";
-import { TitleForm } from "./title-form";
-import { DescriptionForm } from "./description-form";
-import { ImageForm } from "./image-form";
-import { CategoryForm } from "./category-form";
-import { ChaptersForm } from "./chapters-form";
-import { PriceForm } from "./price-form";
-import { AttachmentForm } from "./attachment-form";
-import { Icon } from "@tremor/react";
-import { InstructorNameForm } from "./instructor-name-form";
-import { ThisCourseIsForForm } from "./this-course-is-for-form";
-import { TechnologiesForm } from "./technologies-form";
-import { WhatYouWillLearnForm } from "./what-you-will-learn-form";
-import { PrerequisitsForm } from "./prerequisits-form";
-import { Label } from "@/components/ui/label";
+import { Course } from "@prisma/client";
 import { Switch } from "@/components/ui/switch";
-import { SectionsForm } from "./sections-form";
-import { CourseChaptersSkeleton } from "@/app/(course)/courses/[id]/courseContent/skeletons";
 import { z } from "zod";
 import {
   Form,
@@ -43,31 +10,31 @@ import {
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import PublisCourseForm from "./_components/course-publish-form";
 
 type PublishCourseFormProps = {
   course: Course;
 };
 
 const formSchema = z.object({
-  isPublished: z.boolean().optional().default(true),
+  isPublished: z.boolean(),
 });
 
-const PublishCourseForm = ({
-  course,
-}: PublishCourseFormProps) => {
+const PublishCourseForm = ({ course }: PublishCourseFormProps) => {
   const form = useForm({
     resolver: zodResolver(formSchema),
-    defaultValues: { isPublished: course.isPublished },
+    defaultValues: {
+      isPublished: course?.isPublished ?? true,
+    },
   });
 
   const { isSubmitting, isValid } = form.formState;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    if (!values) return;
+
     try {
       const updatedCourse = { ...course, isPublished: values.isPublished };
       // await updateCourse(updatedCourse);
