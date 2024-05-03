96e82cb83c1f5676528a0201575bddd1b7c175a4
amangit1314 <gitaman8481@gmail.com>
2024-05-03 09:38:22 +0530

    fix: stripe version changed and course progress api no module found error solved

--- src/app/api/profile/[userId]/enrolledCourses/[courseId]/sections/[sectionId]/chapters/[chapterId]/progress/route.ts
+++ src/app/api/profile/[userId]/enrolledCourses/[courseId]/sections/[sectionId]/chapters/[chapterId]/progress/route.ts
@@ -1,9 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
 export const dynamic = 'force-dynamic';
 
-const GET = () => {
+export const GET = (req: NextRequest) => {
   try {
-
+    return NextResponse.json({
+      success: true,
+      status: 'OK',
+      message: 'Success',
+    }, { status: 200 })
   } catch (error) {
-
+    return NextResponse.json({
+      success: false,
+      status: 'ERROR',
+      message: 'Internal server error ...',
+    }, { status: 500 });
   }
 }
