92600426032ca8671d42decb1c8f186076e6c352
amanGit1314 <gitaman8481@gmail.com>
2024-05-25 16:39:53 +0530

    ui: forgot password ui updated

--- src/app/(auth)/(forgotPassword)/resetPassword/page.tsx
+++ src/app/(auth)/(forgotPassword)/resetPassword/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from "react";
 import {
@@ -68,67 +68,86 @@ const ResetPassword = () => {
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 my-12 rounded-3xl bg-gray-900 p-4 flex justify-center items-center align-middle mx-auto ">
-        {/* new password  */}
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <div>
-              <FormItem className="mt-8">
-                <FormLabel className="my-4 text-base text-gray-800 dark:text-gray-100">
-                  New Password
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    disabled={isSubmitting}
-                    className="bg-transparent border-gray-700 dark:border-gray-400 "
-                    placeholder="your password here ..."
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription className="">
-                  Enter your new password
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            </div>
-          )}
-        />
+    <div className="bg-gray-800 py-16 max-h-screen h-full w-full ">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className=" max-w-3xl w-full space-y-4  rounded-3xl bg-gray-900 p-8 flex flex-col align-middle mx-auto "
+        >
+          <div>
+            <h1 className="text-xl font-semibold tracking-tight">
+              Reset Password
+            </h1>
+            <p className="text-sm">
+              Enter you new password and confirm it to change you coursewave
+              password
+            </p>
+          </div>
 
-        {/* confirm password */}
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <div>
-              <FormItem className="mt-8">
-                <FormLabel className="my-4 text-base text-gray-800 dark:text-gray-100">
-                  Confirm Password
-                </FormLabel>
-                <FormControl>
-                  <Input
-                    disabled={isSubmitting}
-                    className="bg-transparent border-gray-700 dark:border-gray-400 "
-                    placeholder="enter your password here agian to confirm ..."
-                    {...field}
-                  />
-                </FormControl>
-                <FormDescription className="">
-                  Enter your new password again to confirm
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            </div>
-          )}
-        />
+          {/* new password  */}
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <div>
+                <FormItem className="">
+                  <FormLabel className="my-4 text-base text-gray-800 dark:text-gray-100">
+                    New Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      className="bg-transparent border-gray-700 dark:border-gray-400 "
+                      placeholder="your password here ..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="">
+                    Enter your new password
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              </div>
+            )}
+          />
 
-        <Button type="submit" disabled={!isValid || isSubmitting} color="blue">
-          Change Password
-        </Button>
-      </form>
-    </Form>
+          {/* confirm password */}
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <div>
+                <FormItem className="">
+                  <FormLabel className="my-4 text-base text-gray-800 dark:text-gray-100">
+                    Confirm Password
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      className="bg-transparent border-gray-700 dark:border-gray-400 "
+                      placeholder="enter your password here agian to confirm ..."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription className="">
+                    Enter your new password again to confirm
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              </div>
+            )}
+          />
+
+          <Button
+            type="submit"
+            disabled={!isValid || isSubmitting}
+            color="blue"
+          >
+            Change Password
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
