d665e468ca97403a2aa1ca9e94887257602e07f0
amanGit1314 <gitaman8481@gmail.com>
2024-05-25 12:12:52 +0530

    feat: forgot password implementation

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,13 @@
+import { Suspense } from "react";
+
 interface AuthLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-    return <div className="min-h-screen">{children}</div>
+  return (
+    <div className="min-h-screen">
+      <Suspense>{children}</Suspense>
+    </div>
+  );
 }
