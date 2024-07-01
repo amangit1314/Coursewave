0934eb3f2b6a0ea4f4fb6e6ea72b7435d5c41286
amanGit1314 <gitaman8481@gmail.com>
2024-07-01 17:28:03 +0530

    feat: save article store and button click

--- src/app/(course)/courses/[id]/page.tsx
+++ src/app/(course)/courses/[id]/page.tsx
@@ -41,7 +41,10 @@ import { SiCrowdsource } from "react-icons/si";
 import { GoProjectTemplate } from "react-icons/go";
 import useInstructorInfo from "@/hooks/use-instructor-info";
 import { VscPreview } from "react-icons/vsc";
-import CourseEnrollButton from "./_components/course-enroll-button";
+import useCheckCourseIsPurchased from "@/hooks/use-check-course-is-puchased";
+import useNotificationsStore from "@/zustand/notificationsStore";
+import axios from "axios";
+import { Loader } from "lucide-react";
 
 function CoursePreview({ params }: { params: { id: string } }) {
   const courseId = params?.id;
@@ -690,6 +693,80 @@ function CourseDescription({
     </div>
   );
 }
+
+ function CourseEnrollButton({
+  course,
+  courseId,
+}: {
+  course: Course;
+  courseId: string;
+}) {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const user = useUserInfo();
+  const isCoursePurchased = useCheckCourseIsPurchased(user?.user?.id, courseId);
+  const setNotification = useNotificationsStore(
+    (state) => state.setNotification
+  );
+
+  const enrollInCourse = async () => {
+    try {
+      setIsLoading(true);
+      console.log(
+        "Is course already purchased? : ",
+        isCoursePurchased.courseIsPurchased
+      );
+      if (isCoursePurchased.courseIsPurchased) {
+        window.location.assign(
+          absoluteUrl(`/courses/${courseId}/courseContent`)
+        );
+      } else {
+        const response = await axios.post(`api/courses/${courseId}/checkout`, {
+          userId: user?.user?.id!,
+        });
+
+        window.location.assign(response.data.url);
+
+        setNotification(
+          "Course Enrollment Successful 🎉",
+          `Congratulations! You have successfully enrolled in "${course?.courseTitle}" course?.`
+        );
+
+        toast.success(
+          `Congratulations 🎉! You have successfully enrolled in "${course?.courseTitle}" course`
+        );
+      }
+    } catch (error) {
+      toast.error("Something went wrong ...");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  if (isCoursePurchased.error) {
+    return <div>Error: {isCoursePurchased.error.message}</div>;
+  }
+
+  return (
+    <div className="flex justify-between">
+      <Button
+        onClick={enrollInCourse}
+        disabled={isLoading}
+        size="sm"
+        color="blue"
+        className="mt-2 text-center text-white bg-blue-500 w-[28rem] md:w-[26rem] mr-8 md:mr-0 rounded-md hover:bg-blue-700 text-sm  font-semibold p-2"
+      >
+        {isCoursePurchased.isLoading ? (
+          <Loader className="animate-spin" />
+        ) : isCoursePurchased.courseIsPurchased ? (
+          "Resume Learning"
+        ) : (
+          "Buy Now"
+        )}
+      </Button>
+    </div>
+  );
+}
+
 
 function AddToCartButton({ course }: { course: Course }) {
   const user = useUserInfo();
