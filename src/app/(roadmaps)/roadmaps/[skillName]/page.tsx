953472c2dbc1083e25ed1dc8c843dde718d6a11f
amanGit1314 <gitaman8481@gmail.com>
2024-10-02 00:11:53 +0530

    fix: skillName roadmap page.tsx no module found error solved

---
+++ src/app/(roadmaps)/roadmaps/[skillName]/page.tsx
@@ -0,0 +1,11 @@
+import React, { Suspense } from "react";
+
+const RoadMapPage = () => {
+  return (
+    <div className="flex h-full">
+      <Suspense>RoadMaps</Suspense>
+    </div>
+  );
+};
+
+export default RoadMapPage;
